6fbbf5fc8c4f15aa0a1bc8ee19c5cc3d6198ae41
Joseph DeBroux <josephdebroux@SDInstructor1.local>
2019-07-19 17:21:11 -0600

    added more hello world

--- src/com/skilldistillery/lordoftheobjects/LordOfTheObjectsApp.java
+++ src/com/skilldistillery/lordoftheobjects/LordOfTheObjectsApp.java
@@ -4,6 +4,10 @@ public class LordOfTheObjectsApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
